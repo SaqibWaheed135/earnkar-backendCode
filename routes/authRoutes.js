af36a074a00ea65357db5cf0655828068e76effb
Saqib Waheed <129432131+SaqibWaheed135@users.noreply.github.com>
2025-06-28 19:48:20 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,7 @@ router.get('/verify-reset-token/:token',verifyResetToken);
 router.post('/reset-password', resetPassword);
 
 // Protected route for changing password when logged in
-//router.post('/change-password', changePassword);
+router.post('/change-password', changePassword);
 
 router.get('/profile', protect, getProfile);
 router.get('/getUsers', getAllUsers);
