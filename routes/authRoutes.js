260f5a9b5aadccd25bd95085d1dd9e65b2711cf4
Saqib Waheed <129432131+SaqibWaheed135@users.noreply.github.com>
2025-06-30 14:37:49 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,9 +21,9 @@ router.put('/profile/image', protect, uploadProfileImage);
 router.post('/reward', reward);
 router.post('/clerk-user', clerkUserHandler);
 router.post('/googleSignIn',googleSignIn);
-router.post('/withdraw', protect, withdraw);
+router.post('/withdraw',withdraw);
 // router.post('/withdrawals/:id/complete', withdrawCompletion);
-router.post('/withdrawals/:id/complete', protect, withdrawCompletion);
+router.post('/withdrawals/:id/complete', withdrawCompletion);
 router.get('/withdrawals', getWithdrawals);
 router.post('/likeVideo', likeVideo); // 🔥 add this route
 router.post('/videos/:id/comment', CommentVideo);
